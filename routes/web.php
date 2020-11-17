9a340ba030b385104f120be0cda2d7ed3e1e3b36
felipe.bertanha.pra <felipe.bertanha@capitaletecnologia.com.br>
2020-11-16 22:28:04 -0300

    aula 1.4 routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+
+
+$router->group(['prefix' => '/api'], function ($router) {
+    $router->get('/series', 'SeriesController@index');
+});
